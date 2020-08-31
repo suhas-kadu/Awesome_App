f38ce7752b4856cef12c3a6cfe7e1764fe55a5be
Suhas Kadu <kadusuhas72@gmail.com>
2020-08-31 18:50:48 +0530

    ch1 completed

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  //WidgetsApp
+  //MaterialApp
+  //CupertinoApp
+
+  //runApp(MaterialApp());
+  //runApp(CupertinoApp());
+  //runApp(WidgetsApp());
+
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(), 
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+    ),
+    ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Awesome App'),
+      ),
+      body: Container(
+        child: Text('Hi Flutter'),
+      ),
+    );
+  }
+}
