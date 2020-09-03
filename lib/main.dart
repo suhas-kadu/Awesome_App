93c4a87524969520fd5bd96773801da83ad8b15c
Suhas Kadu <kadusuhas72@gmail.com>
2020-09-03 16:22:21 +0530

    ch5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+//import 'dart:html';
+
+//import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -29,76 +33,64 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          margin: EdgeInsets.all(20),
-          height: 400,
-          width: 400,
-          color: Colors.blue,
+          color: Colors.red,
+          height: 100,
+          width: 100,
           alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, //spaceAround,//spaceBetween,//center,//end,//start
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-            Container(
-            color: Colors.red,
-            padding: const EdgeInsets.all(20),
-            height: 100,
-            width: 100,
-            alignment: Alignment.center,
-            child: Text(
-              "I'm a box",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
+          child: Text(
+            "I'm a Container",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
             ),
-          ),
-          
-           Container(
-            color: Colors.green,
-            padding: const EdgeInsets.all(20),
-            height: 100,
-            width: 100,
-            alignment: Alignment.center,
-            child: Text(
-              "I'm a box",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-
-           Container(
-            color: Colors.yellow,
-            padding: const EdgeInsets.all(20),
-            height: 100,
-            width: 100,
-            alignment: Alignment.center,
-            child: Text(
-              "I'm a box",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 20,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-
-
-            ],
-
-          
-
-
-
+            textAlign: TextAlign.center,
           ),
         ),
       ),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Brie Larson"),
+              accountEmail: Text("brielarson73@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Brie Larson"),
+              subtitle: Text("Actor"),
+              trailing: Icon(Icons.edit),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("brielarson73@gmail.com"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+
+      //FloatingActionButton
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+        elevation: 5,
+        //mini: true,
+        backgroundColor: Colors.green,
+        hoverColor: Colors.red,
+        hoverElevation: 5,
+      ),
+
+    
+      //floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
